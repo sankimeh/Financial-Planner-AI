764dd218ed3b3c3582398a058b7b5e6a88c8e3b0
sankimeh <awsomesankalp@gmail.com>
2025-05-11 18:18:55 +0530

    Adding Goal Suggester

--- financial-planner/backend/app/core/risk_scoring.py
+++
@@ -1,7 +0,0 @@
-def calculate_risk_profile(user_profile):
-    if user_profile.age < 30:
-        return "High Risk"
-    elif 30 <= user_profile.age < 50:
-        return "Medium Risk"
-    else:
-        return "Low Risk"
